c14f5f9f29dd6c5f43ac1ba2e5eef196f4d7f1ec
CannibalChoco <fudgeblimp@gmail.com>
2017-05-29 18:56:37 +0300

    DiningActivity functional

--- app/src/main/java/com/example/android/liepajatourguide/DiningActivity.java
+++ app/src/main/java/com/example/android/liepajatourguide/DiningActivity.java
@@ -2,6 +2,9 @@ package com.example.android.liepajatourguide;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Emils on 25.05.2017.
@@ -11,6 +14,29 @@ public class DiningActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dining);
+        setContentView(R.layout.list);
+
+        /**
+         * Create a list of attractions in Active Leisure category
+         */
+        final ArrayList<Attraction> attractions = new ArrayList<Attraction>();
+        attractions.add(new Attraction(R.string.reastaurant_piano, R.string.reastaurant_piano_description,
+                R.drawable.restaurant_piano));
+        attractions.add(new Attraction(R.string.house_of_food, R.string.house_of_food_description,
+                R.drawable.house_of_food));
+        attractions.add(new Attraction(R.string.restaurant_libava, R.string.restaurant_libava_description,
+                R.drawable.restaurant_libava));
+        attractions.add(new Attraction(R.string.restaurant_oskars, R.string.restaurant_oskars_description,
+                R.drawable.restaurant_oskars));
+        attractions.add(new Attraction(R.string.restaurant_upe, R.string.restaurant_upe_description,
+                R.drawable.restaurant_upe));
+        attractions.add(new Attraction(R.string.restaurant_pastnieka_maja,
+                R.string.restaurant_pastnieka_maja_description, R.drawable.restaurant_pastnieka_maja));
+        attractions.add(new Attraction(R.string.restaurant_olive, R.string.restaurant_olive_description,
+                R.drawable.restaurant_olive));
+
+        AttractionAdapter adapter = new AttractionAdapter(this, attractions);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
