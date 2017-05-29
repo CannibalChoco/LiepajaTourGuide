45092d45b9631ab168b1fe312ed5473476bd7b30
CannibalChoco <fudgeblimp@gmail.com>
2017-05-29 15:32:22 +0300

    ClassicsActivity now is functional

--- app/src/main/java/com/example/android/liepajatourguide/NatureActivity.java
+++ app/src/main/java/com/example/android/liepajatourguide/NatureActivity.java
@@ -21,13 +21,20 @@ public class NatureActivity extends AppCompatActivity {
          * Create a list of attractions in Active Leisure category
          */
         final ArrayList<Attraction> attractions = new ArrayList<Attraction>();
-        attractions.add(new Attraction(R.string.liepaja_lake, R.string.liepaja_lake_description, R.drawable.liepaja_lake));
-        attractions.add(new Attraction(R.string.liepaja_beach, R.string.liepaja_beach_description, R.drawable.liepaja_beach));
-        attractions.add(new Attraction(R.string.seaside_park, R.string.seaside_park_description, R.drawable.seaside_park));
-        attractions.add(new Attraction(R.string.raina_park, R.string.raina_park_description, R.drawable.raina_park));
-        attractions.add(new Attraction(R.string.southern_breakwater, R.string.southern_breakwater_description, R.drawable.southern_breakwater));
-        attractions.add(new Attraction(R.string.northern_breakwater, R.string.northern_breakwater_description, R.drawable.northern_breakwater));
-        attractions.add(new Attraction(R.string.seaside_park_velo_track, R.string.seaside_park_velo_track_description, R.drawable.velo_track2));
+        attractions.add(new Attraction(R.string.liepaja_lake, R.string.liepaja_lake_description,
+                R.drawable.liepaja_lake));
+        attractions.add(new Attraction(R.string.liepaja_beach, R.string.liepaja_beach_description,
+                R.drawable.liepaja_beach));
+        attractions.add(new Attraction(R.string.seaside_park, R.string.seaside_park_description,
+                R.drawable.seaside_park));
+        attractions.add(new Attraction(R.string.raina_park, R.string.raina_park_description,
+                R.drawable.raina_park));
+        attractions.add(new Attraction(R.string.southern_breakwater, R.string.southern_breakwater_description,
+                R.drawable.southern_breakwater));
+        attractions.add(new Attraction(R.string.northern_breakwater, R.string.northern_breakwater_description,
+                R.drawable.northern_breakwater));
+        attractions.add(new Attraction(R.string.seaside_park_velo_track, R.string.seaside_park_velo_track_description,
+                R.drawable.velo_track2));
 
 
         AttractionAdapter adapter = new AttractionAdapter(this, attractions);
