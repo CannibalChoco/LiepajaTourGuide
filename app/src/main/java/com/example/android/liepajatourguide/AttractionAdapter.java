532fb52d98115ecef5f92bda7a0ff1131a801fed
CannibalChoco <fudgeblimp@gmail.com>
2017-05-30 19:35:27 +0300

    Implemented ViewHolder

--- app/src/main/java/com/example/android/liepajatourguide/AttractionAdapter.java
+++ app/src/main/java/com/example/android/liepajatourguide/AttractionAdapter.java
@@ -1,12 +1,14 @@
 package com.example.android.liepajatourguide;
 
 import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 
 import java.util.ArrayList;
 
@@ -18,28 +20,36 @@ public class AttractionAdapter extends ArrayAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Check if the existing view is being reused, otherwise inflate the view
+
         View listItemView = convertView;
+        ViewHolder holder; // to reference the child views for later actions
+
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+
+            // cache view fields into the holder
+            holder = new ViewHolder();
+            holder.name = (TextView) listItemView.findViewById(R.id.name);
+            holder.description = (TextView) listItemView.findViewById(R.id.description);
+            holder.image = (ImageView) listItemView.findViewById(R.id.image);
+            listItemView.setTag(holder);
+        }else{
+            holder = (ViewHolder) listItemView.getTag();
         }
 
         // get the Attraction object located at this position in the list
         Attraction currentAttraction = (Attraction) getItem(position);
-
-        // set name
-        TextView name = (TextView) listItemView.findViewById(R.id.name);
-        name.setText(currentAttraction.getNameResourceId());
-
-        // set description
-        TextView description = (TextView) listItemView.findViewById(R.id.description);
-        description.setText(currentAttraction.getDescriptionResourceId());
-
-        // set image
-        ImageView image = (ImageView) listItemView.findViewById(R.id.image);
-        image.setImageResource(currentAttraction.getImageResourceId());
+        holder.name.setText(currentAttraction.getNameResourceId());
+        holder.description.setText(currentAttraction.getDescriptionResourceId());
+        holder.image.setImageResource(currentAttraction.getImageResourceId());
 
         return listItemView;
+    }
+
+    static class ViewHolder {
+        TextView name;
+        TextView description;
+        ImageView image;
     }
 }
